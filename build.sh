6f2363b5dede775f9119c46da9b35cd96fa46c21
Jason Pell <jason@pellcorp.com>
2024-08-03 12:33:15 +1000

    add btt eddy build

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 cd $SCRIPT_DIR
 
-rm -rf outfw
+rm -rf outfw/
 ./_build.sh bed || exit $?
 ./_build.sh noz || exit $?
 ./_build.sh mcu || exit $?
 ./_build.sh btteddy || exit $?
 
+mkdir -p fw/K1/
 rm -rf fw/K1/*.bin
 rm -rf fw/K1/*.uf2
 mv outfw/*.bin fw/K1/
