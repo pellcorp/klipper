b10457525e36631af4f5fa7222240c9c14e23eca
Jason Pell <jason@pellcorp.com>
2026-08-19 08:03:41 +1000

    add ender 3 v3 se C13 firmware config

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd $SCRIPT_DIR
 ./_build.sh btteddy || exit $?
 ./_build.sh host || exit $?
 ./_build.sh creality-42x || exit $?
+./_build.sh e3v3se || exit $?
 
 mv outfw/klipper_host_mcu fw/K1/
 mv outfw/btteddy.uf2 fw/K1/
